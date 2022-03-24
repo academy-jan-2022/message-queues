eefd8b6a315be2bdf804a78a70a32249991d0584
Simon <simonbesteiro@gmail.com>
2022-03-24 19:05:38 +0100

    working

--- Message.Listener/MessageTrigger.cs
+++ Message.Listener/MessageTrigger.cs
@@ -10,9 +10,14 @@ public static class MessageTrigger
 {
     [FunctionName("MessageTrigger")]
     public static async Task RunAsync(
-        [QueueTrigger("api", Connection = "StorageConnectionAppSetting")] string myQueueItem, 
-        ILogger log)
+        [QueueTrigger("api", Connection = "StorageConnectionAppSetting")]
+        string message,
+        ILogger log,
+        string id)
     {
-        log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        log.LogInformation($"C# Queue trigger function processed: {message}");
+        int squaredNumber = Int16.Parse(message) * Int16.Parse(message);
+        log.LogInformation($"Squared number is: {squaredNumber}");
+        log.LogInformation($"{id}");
     }
 }
