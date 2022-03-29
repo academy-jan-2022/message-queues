da0356fa025a89889c6aa97c1e574c0767429892
Sofia Carballo <sofia.carballo@codurance.com>
2022-03-29 11:35:57 +0200

    Add client telemetry for correlation id

--- Message.Listener/Message.cs
+++ Message.Listener/Message.cs
@@ -6,9 +6,11 @@ public class Message
 {
     public int Number;
     public string guid;
+    public Guid correlationId;
 
-    public Message(int number, string guid) {
+    public Message(int number, string guid, Guid correlationId) {
         this.Number = number;
         this.guid = guid;
+        this.correlationId = correlationId;
     }
 } 
